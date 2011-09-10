6dcaa313b5ab0c348fffd534f770ce596bbaeb0c
webnull <webnull.www@gmail.com>
2011-09-10 12:39:53 +0200

    Support for MPlayer2, KMPlayer (Konqueror plugin), GMPlayer (mplayer-gui), GNOME Mplayer

--- src/subgetcore/videoplayers.py
+++ src/subgetcore/videoplayers.py
@@ -7,15 +7,23 @@ def Spawn(Subget, VideoFile, Subtitles):
     Command = ""
 
     if DefaultPlayer == 0:
-        Command = "/usr/bin/xdg-open "+VideoFile+" > /dev/null 2> /dev/null &"
+        Command = "/usr/bin/xdg-open \""+VideoFile+"\" > /dev/null 2> /dev/null &"
     elif DefaultPlayer == 1: # MPlayer
-        Command = "/usr/bin/mplayer "+VideoFile+" -sub "+Subtitles+" > /dev/null 2> /dev/null &"
+        Command = "/usr/bin/mplayer \""+VideoFile+"\" -sub \""+Subtitles+"\" > /dev/null 2> /dev/null &"
     elif DefaultPlayer == 2: # SMPlayer
-        Command = "/usr/bin/smplayer "+VideoFile+" -sub "+Subtitles+" > /dev/null 2> /dev/null &"
+        Command = "/usr/bin/smplayer \""+VideoFile+"\" -sub \""+Subtitles+"\" > /dev/null 2> /dev/null &"
     elif DefaultPlayer == 3: # VLC
-        Command = "/usr/bin/vlc "+VideoFile+" --sub-file "+Subtitles+" > /dev/null 2> /dev/null &"
+        Command = "/usr/bin/vlc \""+VideoFile+"\" --sub-file \""+Subtitles+"\" > /dev/null 2> /dev/null &"
     elif DefaultPlayer == 4: # Totem
-        Command = "/usr/bin/totem "+VideoFile+" > /dev/null 2> /dev/null &"
+        Command = "/usr/bin/totem \""+VideoFile+"\" > /dev/null 2> /dev/null &"
+    elif DefaultPlayer == 5: # MPlayer2 (fork of Mplayer)
+        Command = "/usr/bin/mplayer2 \""+VideoFile+"\" -sub \""+Subtitles+"\" > /dev/null 2> /dev/null &"
+    elif DefaultPlayer == 6: # KMPlayer (Konqueror plugin)
+        Command = "/usr/bin/kmplayer \""+VideoFile+"\" > /dev/null 2> /dev/null &"
+    elif DefaultPlayer == 7: # GMPlayer (mplayer-gui)
+        Command = "/usr/bin/gmplayer \""+VideoFile+"\" -sub \""+Subtitles+"\" > /dev/null 2> /dev/null &"
+    elif DefaultPlayer == 8: # GNOME Mplayer
+        Command = "/usr/bin/gnome-mplayer \""+VideoFile+"\" --subtitle=\""+Subtitles+"\" > /dev/null 2> /dev/null &"
 
     if not Command == "":
         print("Executing: "+Command)
