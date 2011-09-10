e2c1828f4223026f9b79798f915749414bdd9a3a
webnull <webnull.www@gmail.com>
2011-09-10 11:54:42 +0200

    Fixed situation when Video Player was not launched

--- src/subgetcore/videoplayers.py
+++ src/subgetcore/videoplayers.py
@@ -3,7 +3,7 @@
 import os
 
 def Spawn(Subget, VideoFile, Subtitles):
-    DefaultPlayer = Subget.Config['afterdownload']['defaultplayer']
+    DefaultPlayer = int(Subget.Config['afterdownload']['defaultplayer'])
     Command = ""
 
     if DefaultPlayer == 0:
